49d3f2e66995f4dbd367a0c5f8bf876f3814fe23
Axel Dalí <axeldali.gm@gmail.com>
2023-10-27 19:52:30 -0600

    Add sign up functionality

--- login/urls.py
+++ login/urls.py
@@ -2,8 +2,11 @@
 
 from django.urls import path, include
 
+from .views import SignUpView
+
 app_name = 'login'
 
 urlpatterns = [
+    path('signup/', SignUpView.as_view(), name='signup'),
     path('', include('django.contrib.auth.urls')),
 ]
